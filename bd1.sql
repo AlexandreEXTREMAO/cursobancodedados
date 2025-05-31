d025d0327f94b9c300f46ad934cbae771272d49f
alexandreEXTREMAO <alexandrefenix1@hotmail.com>
2025-05-31 20:15:57 -0300

    Adicionado arquivo tabela juncao

---
+++ bd1.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE juncao;
+
+USE juncao;
+
+CREATE TABLE profissoes(
+    id INT NOT NULL AUTO_INCREMENT,
+    cargo VARCHAR(60) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE clientes(
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    data_nascimento DATABASE NOT NULL,
+    telefone VARCHAR(14) NOT NULL,
+    id_profissao int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_profissao) REFERENCES profissoes (id)
+
+);
